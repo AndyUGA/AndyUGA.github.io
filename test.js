9dfda53bf70a37cf33c4f812787897b9a86ca593
Andy <att35663@uga.edu>
2019-04-20 10:37:41 -0400

    Reorganized files and removed unused files

--- test.js
+++
@@ -1,7 +0,0 @@
-function incrementValue()
-{
-    var value = parseInt(document.getElementById('number').value, 10);
-    value = isNaN(value) ? 0 : value;
-    value++;
-    document.getElementById('number').value = value;
-}
